4926327fe1a30ab7111428a5bb2f05d8e17b5ec7
Anatolii Lehkyi <lanot.biz@gmail.com>
2026-03-30 21:13:30 +0300

    use OHDFilesystemUtil

--- OpenHD/ohd_common/src/config_paths.cpp
+++ OpenHD/ohd_common/src/config_paths.cpp
@@ -43,7 +43,7 @@ const char* getConfigBasePath() {
     return "/config/openhd/";
   } else if (platform_debug.is_x20()) {
     return "/config/openhd/";
-  } else if (std::filesystem::exists('/boot/firmware/openhd')) { // Ubuntu 24.04 fix
+  } else if (OHDFilesystemUtil::exists('/boot/firmware/openhd')) { // Ubuntu 24.04 fix
     return '/boot/firmware/openhd/'
   } else {
     return "/boot/openhd/";
