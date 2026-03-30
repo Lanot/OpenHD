95838ccafaaed6f1f11589e3da0447f442a391a7
Anatolii Lehkyi <lanot.biz@gmail.com>
2026-03-30 21:05:02 +0300

    Debug

--- OpenHD/ohd_common/inc/openhd_temporary_air_or_ground.h
+++ OpenHD/ohd_common/inc/openhd_temporary_air_or_ground.h
@@ -40,6 +40,9 @@ const auto FILENAME_ETHERNET =
     std::string(getConfigBasePath()) + "ethernet.txt";
 
 static bool file_air_exists() {
+
+  std::cout << "\n\n\n\nDEBUG: getConfigBasePath() =  " << getConfigBasePath() << "\n\n\n\n" << std::endl;
+
   return OHDFilesystemUtil::exists(FILENAME_AIR);
 }
 
