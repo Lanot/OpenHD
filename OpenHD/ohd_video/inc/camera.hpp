efdb5a8d07a51b63fdf4a081f76144a86669a2db
Anatolii Lehkyi <lanot.biz@gmail.com>
2026-04-07 23:09:19 +0300

    Added more resolutions to `X_CAM_TYPE_USB_GENERIC`

--- OpenHD/ohd_video/inc/camera.hpp
+++ OpenHD/ohd_video/inc/camera.hpp
@@ -370,7 +370,9 @@ struct XCamera {
       // most likely working resolution
       ret.push_back(ResolutionFramerate{640, 480, 30});
       //custom
+      ret.push_back(ResolutionFramerate{640, 480, 60});
       ret.push_back(ResolutionFramerate{1280, 720, 30});
+      ret.push_back(ResolutionFramerate{1280, 720, 60});
       ret.push_back(ResolutionFramerate{1920, 1080, 30});
       // auto is also a good choice on usb
       ret.push_back(ResolutionFramerate{0, 0, 0});
